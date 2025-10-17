9957303327432b47380700628e13d7f95224bb07
linz <mm.mohammadi@mahsan.co>
2025-10-17 15:13:19 +0330

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import {Button} from "@/components";
+
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <Button value={'asda'} />
+    </>
   );
 }
--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -1,6 +1,5 @@
 "use client"
 import React, {FC} from "react";
-import Loading from "@/components/shared/loading/Loading";
 
 export interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
     type?: "button" | "submit";
@@ -22,8 +21,7 @@ const Button: FC<ButtonProps> = ({
     return (
         <button type={type} disabled={isDisabled} {...otherProps}
                 className={`flex items-center gap-1.5 justify-center rounded-lg w-full ${theme} ${size} ${classNames}`}
-        >{isLoading ? <Loading size={size === "btn-md" ? "w-5" : "w-4"} bgColor="text-transparent"
-                               color={isDisabled ? "text-grey-300" : ""}/> : undefined}{icon}{value}</button>
+        >{isLoading ? <></> : undefined}{icon}{value}</button>
     );
 };
 

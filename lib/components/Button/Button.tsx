01a68a2ff7ed0d343b3466bc4f5a0300b0dcb017
linz <mm.mohammadi@mahsan.co>
2025-10-18 05:34:12 +0330

    changes

--- lib/components/Button/Button.tsx
+++ lib/components/Button/Button.tsx
@@ -1,11 +1,16 @@
-import React  from "react";
+import React, {FC} from "react";
 
-
-export const Button: React.FC = () => {
+export interface props extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+  type?: "button" | "submit";
+  value: string;
+}
+export const Button: FC<props> = ({type = "button", value}) => {
 
   return (
     <>
-
+      {value}
     </>
   );
 };
+
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import {Button} from "../lib";
 
 function App() {
 
   return (
     <>
-
+      <Button value = "sadasd" />
     </>
   )
 }

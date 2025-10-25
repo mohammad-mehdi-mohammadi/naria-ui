a342f803b5b42afc52d1d198fb5dc3f7d7ad0424
linz <mm.mohammadi@mahsan.co>
2025-10-25 11:36:47 +0330

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
       // Entry point for the library build
       entry: "lib/index.ts",
       // Function to determine the output file name based on the format
-      fileName: (format) => `naria-ui.${format}.js`,
+      fileName: (format) => `index.${format}.js`,
       // Output formats for the library
       formats: ["es", "cjs"],
     },

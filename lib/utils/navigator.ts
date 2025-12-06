45878bbdd49dde5a0f1c6b8d83c297dc983d15e6
linz <mm.mohammadi@mahsan.co>
2025-12-06 18:59:22 +0330

    changes

--- lib/utils/navigator.ts
+++ lib/utils/navigator.ts
@@ -13,7 +13,6 @@ export const removeNavigation = (state: string) => {
 }
 
 export const addNavigation = (state: string) => {
-    console.log('as3')
     if (!window.location.hash) {
         window.location.hash = state;
     } else {
@@ -60,16 +59,16 @@ export const onHashChanges = (state: string) => {
     useEffect(() => {
         NARIA_URL = window.location.href
         const handleHashChange = (e) => {
-            // console.log("daasda0", window.location.href, NARIA_URL, window.history)
+            console.log("daasda0", window.location.href, NARIA_URL, window.history)
             if (window.location.hash.includes(state)) {
                 if (window.location.hash.length === window.location.hash.indexOf(state) + state.length) {
                     if (window.location.href === NARIA_URL) {
-                        // console.log("daasda1", window.location.href, NARIA_URL)
-                        window.history.replaceState(null, '', `${window.location.origin}${window.location.pathname}/`);
-                        NARIA_URL = `${window.location.origin}${window.location.pathname}/`;
+                        console.log("daasda1", window.location.href, NARIA_URL)
+                        window.history.replaceState(null, '', `${window.location.origin}${window.location.pathname}`);
+                        NARIA_URL = `${window.location.origin}${window.location.pathname}`;
                         setIsHashChanged(true);
                     } else {
-                        // console.log("daasda2", window.location.href, NARIA_URL)
+                        console.log("daasda2", window.location.href, NARIA_URL)
                         setIsHashChanged(false);
                         NARIA_URL = window.location.href;
                     }
